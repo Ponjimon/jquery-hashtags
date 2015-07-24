69e3a2217249de12f140f972d1a38850588ffce8
Simon Nußbaumer <doenervich@gmx.at>
2015-07-24 23:38:41 +0200

    Some changes - Updated jQuery to 2.1.4 and Autosize to 2.0.0 - Placed RegEx in one expression each reducing some code...

--- js/jquery.hashtags.js
+++ js/jquery.hashtags.js
@@ -1,38 +1,28 @@
 /*!
-  jQuery #hashtags v0.2.5
+  jQuery #hashtags v0.5.0
 	(c) 2013 Simon Nussbaumer - admin@thurnax.com
-	updated: 2014-05-12
+	updated: 2015-24-07
 	license: GNU LESSER GENERAL PUBLIC LICENSE
 */
 (function($) {
 	$.fn.hashtags = function() {
 		$(this).wrap('<div class="jqueryHashtags"><div class="highlighter"></div></div>').unwrap().before('<div class="highlighter"></div>').wrap('<div class="typehead"></div></div>');
 		$(this).addClass("theSelector");
-		$(this).autosize({append: "\n"});
+		autosize($(this));
 		$(this).on("keyup", function() {
 			var str = $(this).val();
 			$(this).parent().parent().find(".highlighter").css("width",$(this).css("width"));
 			str = str.replace(/\n/g, '<br>');
 			if(!str.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?#([a-zA-Z0-9]+)/g) && !str.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?@([a-zA-Z0-9]+)/g) && !str.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?#([\u0600-\u06FF]+)/g) && !str.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?@([\u0600-\u06FF]+)/g)) {
-                		if(!str.match(/#([\u0600-\u06FF]+)#/g)) { //arabic support
-					str = str.replace(/#([\u0600-\u06FF]+)/g,'<span class="hashtag">#$1</span>');
+                if(!str.match(/#(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))#/g)) { //arabic support
+					str = str.replace(/#(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))/g,'<span class="hashtag">#$1</span>');
 				}else{
-					str = str.replace(/#([\u0600-\u06FF]+)#([\u0600-\u06FF]+)/g,'<span class="hashtag">#$1</span>');
+					str = str.replace(/#(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))#(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))/g,'<span class="hashtag">#$1</span>');
 				}
-				if(!str.match(/@([\u0600-\u06FF]+)@/g)) {
-					str = str.replace(/@([\u0600-\u06FF]+)/g,'<span class="hashtag">@$1</span>');
+				if(!str.match(/@(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))@/g)) {
+					str = str.replace(/@(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))/g,'<span class="hashtag">@$1</span>');
 				}else{
-					str = str.replace(/@([\u0600-\u06FF]+)@([\u0600-\u06FF]+)/g,'<span class="hashtag">@$1</span>');
-				}
-				if(!str.match(/#([a-zA-Z0-9]+)#/g)) {
-					str = str.replace(/#([a-zA-Z0-9]+)/g,'<span class="hashtag">#$1</span>');
-				}else{
-					str = str.replace(/#([a-zA-Z0-9]+)#([a-zA-Z0-9]+)/g,'<span class="hashtag">#$1</span>');
-				}
-				if(!str.match(/@([a-zA-Z0-9]+)@/g)) {
-					str = str.replace(/@([a-zA-Z0-9]+)/g,'<span class="hashtag">@$1</span>');
-				}else{
-					str = str.replace(/@([a-zA-Z0-9]+)@([a-zA-Z0-9]+)/g,'<span class="hashtag">@$1</span>');
+					str = str.replace(/@(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))@(([a-zA-Z0-9]+)|([\u0600-\u06FF]+))/g,'<span class="hashtag">@$1</span>');
 				}
 			}
 			$(this).parent().parent().find(".highlighter").html(str);
